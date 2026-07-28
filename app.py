411880904ecf37bd699cbb54e34717fd0dfbb91e
ankitaaage2-eng <ankitaaage2@gmail.com>
2026-07-28 04:30:53 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+
+import streamlit as st
+
+st.set_page_config(page_title="Ford Car Prediction")
+
+st.title("Ford Car Prediction")
+
+st.write("Session 24 AIML Assignment")
+
+st.success("App is working successfully!")
